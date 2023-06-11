05e85d687921b931757171669f5a23fcef3f3125
velveteen-rever <velveteen.rever@gmail.com>
2023-06-11 05:43:43 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   pages/contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -1,13 +1,89 @@
 import Head from 'next/head';
 import Header from '@components/header';
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
 
-export default function Home() {
+export default function Contact() {
+  const [submitterName, setSubmitterName] = useState('');
+  const router = useRouter();
+  const confirmationScreenVisible =
+    router.query?.success && router.query.success === 'true';
+  const formVisible = !confirmationScreenVisible;
+
+  const confirmationMessage = (
+    <React.Fragment>
+      <p>
+        Thank you for submitting this form. Someone should get back to you
+        within 24-48 hours.
+      </p>
+
+      <button
+        className='btn btn-primary'
+        onClick={() =>
+          router.replace('/contact', undefined, { shallow: true })
+        }>
+        {' '}
+        Submit Another Response{' '}
+      </button>
+    </React.Fragment>
+  );
+
+  const contactForm = (
+    <form
+      name='contact'
+      method='POST'
+      data-netlify='true'
+      action='contact/?success=true'
+      data-netlify-honeypot='bot-field'>
+      <p hidden>
+        <label>
+          Don't fill this out: <input name='bot-field' />
+        </label>
+      </p>
+      <div className='mb-3 col-xxl-8'>
+        <label
+          htmlFor='emailField'
+          className='form-label'>
+          Email address
+        </label>
+        <input
+          type='email'
+          className='form-control'
+          id='emailField'
+          placeholder='name@example.com'
+          name='emailField'
+        />
+      </div>
+      <div className='mb-3'>
+        <label
+          htmlFor='message'
+          className='form-label'>
+          Example textarea
+        </label>
+        <textarea
+          className='form-control'
+          id='message'
+          type='message'
+          rows='3'
+          name='message'></textarea>
+        <div className='mb-3 pt-3'>
+          <button
+            type='submit'
+            className='btn btn-primary mb-3'
+            name='send'>
+            Send
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+
   return (
-    <div className='container-fluid rounded col-lg-8 p-0 align-content-center bg-light'>
+    <div className='container-fluid rounded col-lg-10 col-xl-8 p-0 align-content-center bg-light'>
       <Header title='Get in touch' />
       <div className='container-fluid p-5'>
         <div className='row'>
-          <div className='conainer-fluid col-sm'>
+          <div className='conainer-fluid col-md'>
             <p className='description'>
               Sed ut perspiciatis unde omnis iste natus error sit voluptatem
               accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
@@ -26,46 +102,8 @@ export default function Home() {
             </p>
           </div>
 
-          <div className='container-fluid col-sm'>
-            <form
-              name='contact'
-              method='POST'
-              data-netlify='true'>
-              <div className='mb-3 col-4'>
-                <label
-                  htmlFor='emailField'
-                  className='form-label'>
-                  Email address
-                </label>
-                <input
-                  type='email'
-                  className='form-control'
-                  id='emailField'
-                  placeholder='name@example.com'
-                  name='emailField'
-                />
-              </div>
-              <div className='mb-3'>
-                <label
-                  htmlFor='emailBodyField'
-                  className='form-label'>
-                  Example textarea
-                </label>
-                <textarea
-                  className='form-control'
-                  id='emailBodyField'
-                  rows='3'
-                  name='emailBody'></textarea>
-                <div className='mb-3 pt-3'>
-                  <input
-                    type='submit'
-                    className='btn btn-primary mb-3'
-                    value='Send'
-                    name='send'
-                  />
-                </div>
-              </div>
-            </form>
+          <div className='container-fluid col-md'>
+            {formVisible ? contactForm : confirmationMessage}
           </div>
         </div>
       </div>
