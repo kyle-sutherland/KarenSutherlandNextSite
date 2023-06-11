71af29d6c004e63858b3d176bd432634aa16858f
velveteen-rever <velveteen.rever@gmail.com>
2023-06-10 22:25:32 -0600

    first prototype

---
+++ pages/profile.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Header from '@components/header'
+
+export default function Home() {
+    return (
+        <div className='content'>
+            <div className="container-fluid rounded col-lg-10 p-0 align-content-center bg-light">
+                <Header title="About Karen Sutherland" />
+                <div className="container-fluid p-4">
+                    <p className="description">
+                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
