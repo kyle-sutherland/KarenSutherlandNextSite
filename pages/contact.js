e1590b322f924490821f8d10478a5ce493a7d3f6
velveteen-rever <velveteen.rever@gmail.com>
2023-06-11 16:14:43 -0600

    Revert "Please enter the commit message for your changes. Lines starting"
    This reverts commit 9f869daa8909b09b806201455aa3e9f1065dbf32.

--- pages/contact.js
+++ pages/contact.js
@@ -63,6 +63,7 @@ export default function Contact() {
         <textarea
           className='form-control'
           id='message'
+          type='message'
           rows='3'
           name='message'></textarea>
         <div className='mb-3 pt-3'>
