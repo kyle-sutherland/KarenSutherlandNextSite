fad4205e3712fa67ef492896570a4693e926c8e2
velveteen-rever <velveteen.rever@gmail.com>
2023-06-18 17:08:33 -0600

    graphics and colors

--- components/layout.js
+++ components/layout.js
@@ -5,13 +5,6 @@ import Head from 'next/head';
 export default function Layout({ children }) {
   return (
     <>
-      <Head>
-        <title>Karen Sutherland Counciling</title>
-        <link
-          rel='icon'
-          href='/favicon.png'
-        />
-      </Head>
       <Navigation />
         <div className='page-container container-fluid rounded col-lg-10 col-xxl-8 px-0 pt-5 align-content-center pb-5'>
         <main>{children}</main>
