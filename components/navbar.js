1face481554669ad8e87e33fb62a40d405fead0e
velveteen-rever <velveteen.rever@gmail.com>
2023-06-18 16:33:31 -0600

    graphics and colors

--- components/navbar.js
+++ components/navbar.js
@@ -3,16 +3,16 @@ import {Parallax} from "react-scroll-parallax";
 
 export default function Navigation() {
     return (
-        <div className='container-fluid p-0 col-lg-10 col-xxl-8 pt-lg-5 rounded sticky-top'>
+        <div className='container-fluid p-0 rounded sticky-top'>
             <Navbar
                 bg='light'
                 expand='lg'
                 className='rounded py-0 navi'>
-                <div className='container-fluid p-0'>
+                <div className='container-fluid col-lg-10 col-xxl-8 p-0'>
                     <Navbar.Brand href='/' className='text-dark p-0'>
                         <img
-                            src='/navbar-graphic.svg'
-                            className='d-inline-block img-fluid rounded-start border-end border-accent bg-primary' height='64px' width='240px' alt=''/>
+                            src='/kslogo-navbar-graphic1.svg'
+                            className='d-inline-block img-fluid border-start border-end border-accent bg-primary' height='64px' width='240px' alt=''/>
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls='basic-navbar-nav'/>
                     <div className='justify-content-end'><Navbar.Collapse id='basic-navbar-nav'>
