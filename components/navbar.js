a44110fe4bc9eb57121301bc0dbcaac43c8d561d
velveteen-rever <velveteen.rever@gmail.com>
2023-06-18 04:52:36 -0600

    graphics and colors

--- components/navbar.js
+++ components/navbar.js
@@ -1,28 +1,31 @@
-import { Nav, Navbar } from 'react-bootstrap';
+import {Nav, Navbar} from 'react-bootstrap';
 import {Parallax} from "react-scroll-parallax";
 
 export default function Navigation() {
-  return (
-      <div className='container-fluid p-0 col-lg-10 col-xxl-8 pt-lg-5 rounded sticky-top'>
-        <Navbar
-          bg='light'
-          expand='lg'
-          className='rounded py-0 navi'>
-          <div className='container-fluid p-0'><Navbar.Brand href='/' className='text-dark'><img
-            src='/navbar-graphic.svg'
-            className='d-inline-block img-fluid rounded' height='64px' width='240px' alt=''/></Navbar.Brand>
-            <Navbar.Toggle aria-controls='basic-navbar-nav'/>
-            <div className='justify-content-end'><Navbar.Collapse id='basic-navbar-nav'>
-              <Nav className='me-auto bg-light px-4'>
-                <Nav.Link href='/'>Home</Nav.Link>
-                <Nav.Link href='/profile'>Profile</Nav.Link>
-                <Nav.Link href='/services'>Services</Nav.Link>
-                <Nav.Link href='/fees'>Fees</Nav.Link>
-                <Nav.Link href='/contact'>Contact</Nav.Link>
-              </Nav>
-            </Navbar.Collapse></div>
-          </div>
-        </Navbar>
-      </div>
-  );
+    return (
+        <div className='container-fluid p-0 col-lg-10 col-xxl-8 pt-lg-5 rounded sticky-top'>
+            <Navbar
+                bg='light'
+                expand='lg'
+                className='rounded py-0 navi'>
+                <div className='container-fluid p-0'>
+                    <Navbar.Brand href='/' className='text-dark p-0'>
+                        <img
+                            src='/navbar-graphic.svg'
+                            className='d-inline-block img-fluid rounded-start border-end border-accent bg-primary' height='64px' width='240px' alt=''/>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls='basic-navbar-nav'/>
+                    <div className='justify-content-end'><Navbar.Collapse id='basic-navbar-nav'>
+                        <Nav className='me-auto bg-light px-4'>
+                            <Nav.Link href='/'>Home</Nav.Link>
+                            <Nav.Link href='/profile'>Profile</Nav.Link>
+                            <Nav.Link href='/services'>Services</Nav.Link>
+                            <Nav.Link href='/fees'>Fees</Nav.Link>
+                            <Nav.Link href='/contact'>Contact</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse></div>
+                </div>
+            </Navbar>
+        </div>
+    );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,79 +1,78 @@
-import Head from 'next/head';
 import Header from '@components/header';
 import {Content, EndNote} from "@components/content";
 import {AdvancedBannerTop} from "@components/advancedBannerTop";
 import {Parallax} from "react-scroll-parallax";
 
 export default function Home() {
-  return (<>
-    <AdvancedBannerTop/>
-    <Parallax opacity={[0, 1]} startScroll={0} endScroll={470} easing={'easeInCubic'}>
-      <Parallax opacity={[1, 0]} startScroll={900} endScroll={1100}><Header title='Karen Sutherland'/>
-        <div className='container-fluid bg-light'>
-          <Content>
-            <p className='lead'>
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-              ab illo inventore veritatis et quasi architecto beatae vitae dicta
-              sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
-              qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
-              dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-            </p>
-          </Content>
-        </div>
-      </Parallax>
-      </Parallax>
-      <div className='cutout container-fluid px-4 bg-transparent text-dark'>
-        <div className='container cutout-text col-8'>
-          <div className='container-fluid'><h1>A bunch of nice text</h1></div>
-          <div className='container-fluid col-xl-6'><h3>
-            This is where you could place some nice, welcoming
-            text about Karen Sutherland. Who doesn't want to
-            know about Karen Sutherland?
-          </h3>
-          </div>
-        </div>
-      </div>
-      <EndNote>
-        <div className='container-fluid col-auto card-row'>
-          <div className='row'>
-            <div className='container-fluid col-auto p-3'>
-              <div className="card text-slate">
-                <img src="..." className="card-img-top" alt="..."/>
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of
-                    the card's content.</p>
-                  <a href="#" className="btn btn-primary text-dark">Go somewhere</a>
+    return (<>
+        {/*<img src='/kslogo-banner-large.svg' className='img-fluid rounded-top bg-primary'/>*/}
+        <div className='container-fluid rounded-top bg-primary'><AdvancedBannerTop/></div>
+                <div className='container-fluid bg-light'>
+                    <Content>
+                        <p className='lead'>
+                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+                            ab illo inventore veritatis et quasi architecto beatae vitae dicta
+                            sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+                            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+                            qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
+                            dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+                        </p>
+                    </Content>
                 </div>
-              </div>
-            </div>
-            <div className='container-fluid col-auto p-3'>
-              <div className="card text-slate">
-                <img src="..." className="card-img-top" alt="..."/>
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of
-                    the card's content.</p>
-                  <a href="#" className="btn btn-primary text-dark">Go somewhere</a>
+        <div className='cutout container-fluid px-4 bg-transparent text-dark'>
+            <div className='container cutout-text col-8'>
+                <div className='container-fluid'><h1>A bunch of nice text</h1></div>
+                <div className='container-fluid col-xl-6'><h3>
+                    This is where you could place some nice, welcoming
+                    text about Karen Sutherland. Who doesn't want to
+                    know about Karen Sutherland?
+                </h3>
                 </div>
-              </div>
             </div>
-            <div className='container-fluid col-auto p-3'>
-              <div className="card text-slate">
-                <img src="..." className="card-img-top" alt="..."/>
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of
-                    the card's content.</p>
-                  <a href="#" className="btn btn-primary text-dark">Go somewhere</a>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
-      </EndNote>
+        <EndNote>
+            <div className='container-fluid col-auto card-row'>
+                <div className='row'>
+                    <div className='container-fluid col-auto p-3'>
+                        <div className="card text-slate">
+                            <img src="..." className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up
+                                    the bulk of
+                                    the card's content.</p>
+                                <a href="#" className="btn btn-primary text-dark">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='container-fluid col-auto p-3'>
+                        <div className="card text-slate">
+                            <img src="..." className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up
+                                    the bulk of
+                                    the card's content.</p>
+                                <a href="#" className="btn btn-primary text-dark">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='container-fluid col-auto p-3'>
+                        <div className="card text-slate">
+                            <img src="..." className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up
+                                    the bulk of
+                                    the card's content.</p>
+                                <a href="#" className="btn btn-primary text-dark">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </EndNote>
 
     </>);
 }
