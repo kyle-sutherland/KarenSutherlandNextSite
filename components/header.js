ec41191b155123937c3204e4471dcfac1f7a906e
velveteen-rever <velveteen.rever@gmail.com>
2023-06-22 12:07:24 -0600

    refinements

--- components/header.js
+++ components/header.js
@@ -1,7 +1,7 @@
 export default function Header({ title }) {
   return (
     <>
-      <div className="container text-center content-header bg-thistle-blossom text-charcoal md:rounded-t-lg border-b border-bosporus">
+      <div className="container text-center content-header bg-thistle-blossom text-maniac-mansion md:rounded-t-lg border-b border-bosporus">
         <p className="text-3xl p-2">{title}</p>
       </div>
     </>
