cf51a4f45e47e31afcc32743e62b91563d6d5bd4
velveteen-rever <velveteen.rever@gmail.com>
2023-08-11 16:20:16 -0600

    refinements

---
+++ components/buttons.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const buttonStyle =
+  "rounded-md bg-bosporus px-3 py-2 text-sm text-bleached-silk shadow-sm hover:bg-opacity-60 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-thistle-blossom";
+
+export function LinkButton({ target, text }) {
+  return (
+    <>
+      <a href={target ? target : "#"} className={buttonStyle}>
+        {text}
+      </a>
+    </>
+  );
+}
+
+export function ActionButton({ type, text }) {
+  return (
+    <>
+      <button className={buttonStyle} type={type}>
+        {text}
+      </button>
+    </>
+  );
+}
