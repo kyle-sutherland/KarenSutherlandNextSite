686544f1156250dced51bb1ef87fa41f24973947
velveteen-rever <velveteen.rever@gmail.com>
2023-08-11 21:45:29 -0600

    refinements

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,19 @@
-import '@styles/globals.scss'
-import Layout from '../components/layout'
+import "@styles/globals.scss";
+import Layout from "../components/layout";
 import Head from "next/head";
 
 function Application({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Karen Sutherland Mindfulness Therapy</title>
-        <link
-          rel='icon'
-          href='/favicon.png'
-        />
+        <title>Karen Sutherland Counseling</title>
+        <link rel="icon" href="/favicon.png" />
       </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
     </>
-  )
+  );
 }
 
 export default Application
