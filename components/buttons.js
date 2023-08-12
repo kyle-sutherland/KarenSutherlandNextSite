575ba02b390a77efa468916042707744df75c645
velveteen-rever <velveteen.rever@gmail.com>
2023-08-11 20:52:56 -0600

    refinements

--- components/buttons.js
+++ components/buttons.js
@@ -3,13 +3,13 @@ import React from "react";
 const buttonStyle =
   "rounded-md bg-bosporus px-3 py-2 text-sm text-bleached-silk shadow-sm hover:bg-opacity-60 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-thistle-blossom";
 
-export function LinkButton({ target, text }) {
+export function LinkButton({ link, text, target, rel }) {
   return (
     <>
       <a
-        href={target ? target : "#"}
-        rel="noopener"
-        target="_blank"
+        href={link ? link : "#"}
+        target={target ? target : ""}
+        rel={rel ? rel : ""}
         className={buttonStyle}
       >
         {text}
