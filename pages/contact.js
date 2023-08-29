a2dec647d7563ed01a2d9d17671c7bfb1733bb6c
velveteen-rever <velveteen.rever@gmail.com>
2023-08-29 15:11:01 -0600

    Changes to be committed: modified:   package-lock.json modified:   package.json modified:   pages/contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -99,7 +99,7 @@ export default function Contact() {
               id="message"
               name="message"
               required
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-4 focus:ring-inset focus:ring-thistle-blossom-light sm:text-sm sm:leading-6"
+             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-4 focus:ring-inset focus:ring-thistle-blossom-light sm:text-sm sm:leading-6"
               rows={3}
             />
           </div>
