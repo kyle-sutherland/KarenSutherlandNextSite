74990a7ae90d0a506412bb8c5d2db1f6c0948ffe
velveteen-rever <velveteen.rever@gmail.com>
2023-08-31 09:48:15 -0600

    Changes to be committed: modified:   next.config.js modified:   package-lock.json modified:   package.json

--- next.config.js
+++ next.config.js
@@ -2,13 +2,5 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-const nextConfig = {
-  i18n: {
-    locales: ["en"], 
-    defaultLocale: "en", 
-  },
-}
-
 module.exports = withBundleAnalyzer({});
-module.exports = nextConfig
 
