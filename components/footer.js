0bf3d76e507d1be5f3c0e4915e19ce0350bfe1df
velveteen-rever <velveteen.rever@gmail.com>
2023-08-31 18:23:07 -0600

    broooo

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (<>
     <footer>
-      <div className="w-full fixed bottom-0 text-center bg-bleached-silk p-1">
+      <div className="z-20 w-full fixed bottom-0 text-center bg-bleached-silk p-1">
         <p className={'text-xs font-light'}>Copyright &copy; Karen Martin Sutherland</p>
       </div>
     </footer>
